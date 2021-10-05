c63016dcdf9ba94de39fa69d8cdfbe6ffaa87c1e
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 11:07:43 +0300

    Edit textfield font

--- Calculator.java
+++ Calculator.java
@@ -5,16 +5,18 @@ public class Calculator {
     JFrame frame;
     JTextField textfield;
 
-    Font myFont = new Font("Serif", Font.BOLD, 30);
+    Font myFont = new Font("Serif", Font.BOLD, 33);
 
     // Constructor
     Calculator() {
+        // Frame: 
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.getContentPane().setBackground(new java.awt.Color(61,44,141));
         frame.setLayout(null);
 
+        // Textfield: 
         textfield = new JTextField();
         textfield.setBounds(40, 25, 340, 50);
         textfield.setFont(myFont);
