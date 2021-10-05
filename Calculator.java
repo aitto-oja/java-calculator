f882c942e1db5be9f24ff7619250ebb96467b30a
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 11:19:56 +0300

    Add a panel to the frame

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Calculator {
     JFrame frame;
     JTextField textfield;
+    JPanel panel;
 
     Font myFont = new Font("Serif", Font.BOLD, 33);
 
@@ -21,6 +22,13 @@ public class Calculator {
         textfield.setBounds(40, 25, 340, 50);
         textfield.setFont(myFont);
         frame.add(textfield);
+
+        // Panel: 
+        panel = new JPanel();
+        panel.setBounds(40, 100, 340, 340);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(new java.awt.Color(145, 107, 191));
+        frame.add(panel);
 
         frame.setVisible(true);
     }
