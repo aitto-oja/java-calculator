f20899c24efa9547c9efe46fced84ec3d84b270b
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 09:59:09 +0300

    Edit JFrame

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,9 @@ public class Calculator {
     // Constructor
     Calculator() {
         frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
     }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
