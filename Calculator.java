6cf2fd7778eb05759ebf6527cb0314050de5c460
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 12:06:40 +0300

    Add function buttons

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,9 @@ public class Calculator {
     JTextField textfield;
     JPanel panel;
     JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
 
     Font myFont = new Font("Serif", Font.BOLD, 33);
 
@@ -31,25 +34,60 @@ public class Calculator {
             numberButtons[i].setFocusable(false);
         }
 
+        // Function Buttons:
+        addButton = new JButton("+");
+        functionButtons[0] = addButton;
+        subButton = new JButton("-");
+        functionButtons[1] = subButton;
+        mulButton = new JButton("*");
+        functionButtons[2] = mulButton;
+        divButton = new JButton("/");
+        functionButtons[3] = divButton;
+        decButton = new JButton(".");
+        functionButtons[4] = decButton;
+        equButton = new JButton("=");
+        functionButtons[5] = equButton;
+        delButton = new JButton("Delete");
+        functionButtons[6] = delButton;
+        clrButton = new JButton("Clear");
+        functionButtons[7] = clrButton;
+        negButton = new JButton("(-)");
+        functionButtons[8] = negButton;
+
+        for (int i=0; i<9; i++) {
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
         // Panel: 
         panel = new JPanel();
         panel.setBounds(40, 100, 340, 340);
         panel.setLayout(new GridLayout(4, 4, 5, 5));
         panel.setBackground(new java.awt.Color(145, 107, 191));
-
+        
+        // First row
         panel.add(numberButtons[1]);
         panel.add(numberButtons[2]);
         panel.add(numberButtons[3]);
+        panel.add(functionButtons[0]);
 
+        // Second row
         panel.add(numberButtons[4]);
         panel.add(numberButtons[5]);
         panel.add(numberButtons[6]);
+        panel.add(functionButtons[1]);
 
+        // Third row
         panel.add(numberButtons[7]);
         panel.add(numberButtons[8]);
         panel.add(numberButtons[9]);
+        panel.add(functionButtons[2]);
 
+        // Fourth row
+        panel.add(functionButtons[4]);
         panel.add(numberButtons[0]);
+        panel.add(functionButtons[5]);
+        panel.add(functionButtons[3]);
 
         frame.add(panel);
 
