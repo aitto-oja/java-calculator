84d70b05cd34649bad1e009a1dc94cee1dde41c9
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 09:56:55 +0300

    Add a JFrame

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 
 public class Calculator {
+    JFrame frame;
+
     // Constructor
     Calculator() {
-
+        frame = new JFrame("Calculator");
     }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
