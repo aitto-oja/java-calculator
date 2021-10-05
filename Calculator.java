cc6fe3681ecd118695ec6df65b48da195ff38ac2
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-10-05 10:09:18 +0300

    Add a textfield to the frame

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Calculator {
     JFrame frame;
+    JTextField textfield;
 
     // Constructor
     Calculator() {
@@ -9,6 +10,10 @@ public class Calculator {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(40, 25, 340, 50);
+        frame.add(textfield);
 
         frame.setVisible(true);
     }
